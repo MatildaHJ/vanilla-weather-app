7bb1dc27e77fa09046344bba2ed9abec00a8750e
MatildaHJ <matilda.hj.larsson@gmail.com>
2023-10-01 17:23:46 +0200

    Added JS today´s day, date and time

--- src/app.js
+++ src/app.js
@@ -1,1 +1,38 @@
+let now = new Date();
+let todaysDate = document.querySelector("#timeDate");
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 
+let months = [
+  "Jan",
+  "Feb",
+  "Mars",
+  "Apr",
+  "May",
+  "June",
+  "July",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+let month = months[now.getMonth()];
+let day = days[now.getDay()];
+let date = now.getDate();
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+timeDate.innerHTML = `${day}, ${month} ${date}, ${hours}:${minutes}`;
